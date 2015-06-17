84c8e9c063ddd4c34abbae816a9a9f1477cfadde
Te Aihe Butler <teaihebutler@gmail.com>
2015-06-18 11:37:18 +1200

    Created migrations and created post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
+
 	has_many :user_followers
   has_many :followers, through: :user_followers
+	has_many :posts
+
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+{2,}\z/}
   validates :email, uniqueness: true, presence: true
   validates :password, length: 5, presence: true
+
   def authenticate(password)
     return true if password == self.password
   end
+  
 end
