a2cabce6b59f5bd835f9d10adc030c9b5690ea45
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-06-18 21:58:17 +1200

    fixed bug with factory girl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,9 +17,6 @@ require 'faker'
 require 'validate_url'
 require 'scrypt'
 
-FactoryGirl.definition_file_paths = %w{./factories ./test/factories ./spec/factories}
-FactoryGirl.find_definitions
-
 RSpec.configure do |config|
   config.include Rack::Test::Methods
   config.include FactoryGirl::Syntax::Methods
