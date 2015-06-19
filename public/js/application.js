6e6e3e80bc42e76a87c230263252b4ec7c324fa6
Kelly Munro <kellymdev@gmail.com>
2015-06-19 13:21:39 +1200

    Initial lightbox working

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,33 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  var $overlay = $('<div id="overlay"></div>');
+  var $close = $("<p>Click to close</p>");
+  var $image = $("<img>");
+  var $caption = $("<p></p>");
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $close.css({'text-align': 'right',
+              'margin-right': '20px',
+              'font-size': '0.75em'});
+
+  $overlay.append($close);
+
+  $overlay.append($image);
+
+  $("body").append($overlay);
+
+  $overlay.append($caption);
+
+  $(".image a").click(function(event){
+    event.preventDefault();
+    var imageLocation = $(this).attr("href");
+    $image.attr("src", imageLocation);
+
+    $overlay.show();
+
+    var captionText = $(this).children("img").attr("alt");
+    $caption.text(captionText);
+  });
+
+  $overlay.click(function() {
+    $overlay.hide();
+  });
 });
